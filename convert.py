ee8e231d576aa32b3dc006d3ad6c3b7da9438810
Tim Hung <thung1@binghamton.edu>
2017-01-06 13:48:47 -0500

    Add more pythony stuff

--- convert.py
+++ convert.py
@@ -1,7 +1,8 @@
-# Terraria chat message tags can be used to display items in chat.
-# This is done with the format: [i:<unique_item_id>]
-# There exist statues in the game that depict alphanumeric characters.
-# This program conveniently converts plaintext into statue chat tags.
+"""Terraria chat message tags can be used to display items in chat.
+This is done with the format: [i:<unique_item_id>]
+
+There exist statues in the game that depict alphanumeric characters.
+This program conveniently converts plaintext into statue chat tags."""
 
 import sys
 
@@ -11,20 +12,21 @@ CONST_BIG_0 = 2702 - ord('0')
 # Big A Statue Item ID offset by ASCII A
 CONST_BIG_A = 2712 - ord('A')
 
-# Converts a char to the string code for an equivalent big letter statue
+def formatTag(itemID):
+  """Return a valid item chat tag format given an ID"""
+  return "[i:{}]".format(str(itemID))
+
 def charToBig(c):
+  """Convert a char to the string code for an equivalent big letter statue"""
   if c.isdigit():
-    return "[i:" + str(ord(c) + CONST_BIG_0) + "]"
+    return formatTag(ord(c) + CONST_BIG_0)
   if c.isalpha():
-    return "[i:" + str(ord(c) + CONST_BIG_A) + "]"
+    return formatTag(ord(c) + CONST_BIG_A)
   return c
 
-# Converts a string to the codes for equivalent big letter statues
 def strToBig(raw):
-  output = ""
-  for c in raw.upper():
-    output += charToBig(c)
-  return output
+  """Convert a string to the codes for equivalent big letter statues."""
+  return ''.join(charToBig(c) for c in raw.upper())
 
 # Main
 if len(sys.argv) > 1:
